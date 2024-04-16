a80416cf14d3515280cf59b51b027e977345de62
eensungkim <eensungkim@gmail.com>
2024-04-16 15:25:26 +0900

    feat: 세션 및 뷰 구성

--- AR-GPT-ImageDetection/AR-GPT-ImageDetection/ImageDetection/Controller/AGIImageDetectionViewController.swift
+++ AR-GPT-ImageDetection/AR-GPT-ImageDetection/ImageDetection/Controller/AGIImageDetectionViewController.swift
@@ -5,12 +5,38 @@
 //  Created by Kim EenSung on 4/16/24.
 //
 
+import ARKit
 import UIKit
 
-class AGIImageDetectionViewController: UIViewController {
-
+/// AGI 앱의 이미지 인식 탭을 담당하는 컨트롤러
+final class AGIImageDetectionViewController: UIViewController {
+    private let session = ARSession()
+    private let imageDetectionView = ARSCNView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+        
+        configureSession()
+        configureImageDetectionView()
+    }
+    
+    override func loadView() {
+        view = imageDetectionView
+    }
+}
+
+// MARK: - Configuration
+extension AGIImageDetectionViewController: ARSessionDelegate, ARSCNViewDelegate {
+    private func configureSession() {
+        session.delegate = self
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.maximumNumberOfTrackedImages = 4
+        session.run(configuration)
+    }
+    
+    private func configureImageDetectionView() {
+        imageDetectionView.delegate = self
+        imageDetectionView.session = session
     }
 }
