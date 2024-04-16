21a6d396631619bb878de5dff4338799850d3f6f
eensungkim <eensungkim@gmail.com>
2024-04-16 13:37:48 +0900

    config: 프로젝트 생성

---
+++ AR-GPT-ImageDetection/AR-GPT-ImageDetection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AR-GPT-ImageDetection
+//
+//  Created by Kim EenSung on 4/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
