a4dc50c2e2816166e75fb7e29839ce16e59f784c
eensungkim <eensungkim@gmail.com>
2024-04-16 14:20:34 +0900

    feat: 이미지 등록 컨트롤러, 이미지 인식 컨트롤러 생성

---
+++ AR-GPT-ImageDetection/AR-GPT-ImageDetection/ImageDetection/Controller/AGIImageDetectionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AGIImageDetectionViewController.swift
+//  AR-GPT-ImageDetection
+//
+//  Created by Kim EenSung on 4/16/24.
+//
+
+import UIKit
+
+class AGIImageDetectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ AR-GPT-ImageDetection/AR-GPT-ImageDetection/MarkerRegistration/Controller/AGIMarkerRegistrationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AGIMarkerRegistrationViewController.swift
+//  AR-GPT-ImageDetection
+//
+//  Created by Kim EenSung on 4/16/24.
+//
+
+import UIKit
+
+class AGIMarkerRegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
