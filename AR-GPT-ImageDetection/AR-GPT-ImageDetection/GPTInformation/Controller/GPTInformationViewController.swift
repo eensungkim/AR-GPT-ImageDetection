f2a5036357ca4fb9809d29da4ae3c98d63d2f4b8
eensungkim <eensungkim@gmail.com>
2024-04-18 15:09:50 +0900

    feat: GPTInformationViewController 구현

--- AR-GPT-ImageDetection/AR-GPT-ImageDetection/GPTInformation/Controller/GPTInformationViewController.swift
+++ AR-GPT-ImageDetection/AR-GPT-ImageDetection/GPTInformation/Controller/GPTInformationViewController.swift
@@ -7,23 +7,48 @@
 
 import UIKit
 
-class GPTInformationViewController: UIViewController {
+final class GPTInformationViewController: UIViewController {
+    private let textLabel: UILabel = {
+        let textLabel = UILabel()
+        textLabel.font = .preferredFont(forTextStyle: .body)
+        textLabel.numberOfLines = 0
+        textLabel.lineBreakStrategy = .hangulWordPriority
+        return textLabel
+    }()
+    private let loadingIndicatorView = UIActivityIndicatorView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureUI()
+        loadingIndicatorView.startAnimating()
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension GPTInformationViewController {
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(loadingIndicatorView)
+        view.addSubview(textLabel)
+        
+        loadingIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            loadingIndicatorView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            loadingIndicatorView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        ])
+        
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            textLabel.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.9),
+            textLabel.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.9),
+            textLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            textLabel.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        ])
     }
-    */
+}
 
+extension GPTInformationViewController {
+    func setText(_ text: String) {
+        loadingIndicatorView.stopAnimating()
+        textLabel.text = text
+    }
 }
