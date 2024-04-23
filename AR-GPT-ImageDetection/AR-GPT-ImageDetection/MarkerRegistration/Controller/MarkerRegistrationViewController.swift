707d853fac7d516d84fe8b26faf9d5ce749824ca
eensungkim <eensungkim@gmail.com>
2024-04-23 17:36:52 +0900

    refactor: 마커등록 뷰에서 이미지피커 생성, 호출하도록 리팩토링

--- AR-GPT-ImageDetection/AR-GPT-ImageDetection/MarkerRegistration/Controller/MarkerRegistrationViewController.swift
+++ AR-GPT-ImageDetection/AR-GPT-ImageDetection/MarkerRegistration/Controller/MarkerRegistrationViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 final class MarkerRegistrationViewController: UIViewController {
     
     private let markerImageManager = MarkerImageManager(container: MarkerImageCoreData.shared.persistentContainer)
+    private let markerRegistrationView = MarkerRegistrationView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,6 @@ final class MarkerRegistrationViewController: UIViewController {
     }
     
     private func setupMarkerRegistrationView() {
-        let markerRegistrationView = MarkerRegistrationView()
         view = markerRegistrationView
         markerRegistrationView.addTarget(self, method: #selector(togglePhotoLibrary))
     }
@@ -37,7 +37,26 @@ final class MarkerRegistrationViewController: UIViewController {
     }
     
     @objc private func togglePhotoLibrary() {
-        let photoUploadViewController = PhotoUploadViewController()
-        self.present(photoUploadViewController, animated: true)
+        presentImagePicker()
+    }
+}
+
+extension MarkerRegistrationViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    private func presentImagePicker() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            // 선택된 이미지 처리
+        }
+        picker.dismiss(animated: true)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
     }
 }
