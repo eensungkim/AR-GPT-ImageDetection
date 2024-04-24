97464d7d79bf931e1ecec594b5607e4d6758e1e7
eensungkim <eensungkim@gmail.com>
2024-04-24 17:13:06 +0900

    refactor: 유저가 직접 등록한 이미지를 referenceImage 로 활용하도록 기능 리팩토링

--- AR-GPT-ImageDetection/AR-GPT-ImageDetection/ImageDetection/Model/MarkerProvider.swift
+++ AR-GPT-ImageDetection/AR-GPT-ImageDetection/ImageDetection/Model/MarkerProvider.swift
@@ -11,9 +11,16 @@ struct MarkerProvider {
     static var markerImageSet: [String: MarkerImage] = [:]
     
     static func loadMarkerImages() -> Set<ARReferenceImage> {
-        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "MarkerImages", bundle: nil) else {
-            return []
-        }
+        let manager = MarkerImageManager.shared
+        let referenceImages: Set<ARReferenceImage> = Set(manager.fetch().compactMap { markerImage in
+            markerImageSet[markerImage.name] = markerImage
+            guard let image = UIImage(data: markerImage.data)?.cgImage else {
+                return nil
+            }
+            let referenceImage = ARReferenceImage(image, orientation: .up, physicalWidth: 0.2)
+            referenceImage.name = markerImage.name
+            return referenceImage
+        })
         
         return referenceImages
     }
