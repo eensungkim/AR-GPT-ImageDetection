68c25c765fc801b331d937b2a8271366af622650
eensungkim <eensungkim@gmail.com>
2024-04-24 11:35:39 +0900

    feat: 이미지 리사이징 메서드 구현

---
+++ AR-GPT-ImageDetection/AR-GPT-ImageDetection/Foundation/UIImage++.swift
@@ -0,0 +1,25 @@
+//
+//  UIImage++.swift
+//  AR-GPT-ImageDetection
+//
+//  Created by Kim EenSung on 4/24/24.
+//
+
+import UIKit
+
+extension UIImage {
+    func resizeImage(toWidth width: CGFloat) -> UIImage? {
+        let originalSize = self.size
+        let scale = width / originalSize.width
+        let newHeight = originalSize.height * scale
+        let newSize = CGSize(width: width, height: newHeight)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        self.draw(in: CGRect(origin: .zero, size: newSize))
+        
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return resizedImage
+    }
+}
