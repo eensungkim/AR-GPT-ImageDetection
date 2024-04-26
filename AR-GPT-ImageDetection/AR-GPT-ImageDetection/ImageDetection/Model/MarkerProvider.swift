3e140c52403f4ada66e669dbed4b74fc6ab8c7d5
eensungkim <eensungkim@gmail.com>
2024-04-26 13:25:09 +0900

    refactor: UUID 로 ARReferenceImage 를 식별하도록 수정

--- AR-GPT-ImageDetection/AR-GPT-ImageDetection/ImageDetection/Model/MarkerProvider.swift
+++ AR-GPT-ImageDetection/AR-GPT-ImageDetection/ImageDetection/Model/MarkerProvider.swift
@@ -13,19 +13,19 @@ struct MarkerProvider {
     static func loadMarkerImages() -> Set<ARReferenceImage> {
         let manager = MarkerImageManager.shared
         let referenceImages: Set<ARReferenceImage> = Set(manager.fetchAll().compactMap { markerImage in
-            markerImageSet[markerImage.name] = markerImage
+            markerImageSet[markerImage.id.uuidString] = markerImage
             guard let image = UIImage(data: markerImage.data)?.cgImage else {
                 return nil
             }
             let referenceImage = ARReferenceImage(image, orientation: .up, physicalWidth: 0.2)
-            referenceImage.name = markerImage.name
+            referenceImage.name = markerImage.id.uuidString  // 중복되지 않는 값인 UUID 로 식별
             return referenceImage
         })
         
         return referenceImages
     }
     
-    static func getMetaData(by name: String) -> MarkerImage? {
-        return markerImageSet[name]
+    static func getMetaData(by id: String) -> MarkerImage? {
+        return markerImageSet[id]
     }
 }
